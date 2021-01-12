124515fd9cc3b0db7d35be4dd0123e03c1ae337b
nikhilDevarasetty <nicholus158@gmail.com>
2021-01-12 17:56:06 +0530

    testing

--- src/models/RefreshToken.js
+++ src/models/RefreshToken.js
@@ -4,7 +4,6 @@ const mongooose = require("mongoose");
 const tokenSchema = new mongooose.Schema({
   // Your code goes here
   token: String,
-  user_id: String,
 });
 
 const RefreshToken = mongooose.model("tokens", tokenSchema);
